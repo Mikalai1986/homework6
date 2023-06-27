323cff4a5a5143bdd2d6dda23aa6be3ddd95ba57
FostikN <fostik@tut.by>
2023-06-27 14:16:16 +0300

    решение задачи 41

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Пользователь вводит с клавиатуры М чисел. Посчитайте сколько чисел больше 0 ввел пользователь
+int Inputnum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int M = Inputnum("Введите число М: ");
+int ind = 0;
+for (int i = 0; i < M; i++)
+    {
+    int N = int.Parse(Console.ReadLine()!);
+    if (N > 0) ind++;
+
+    }
+
+Console.WriteLine($"Количество положительных введенных = {ind}");
